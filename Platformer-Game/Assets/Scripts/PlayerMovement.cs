7d20dfc79d584e04b0c12cdd0652153b817f414c
Luth <noafowl@gmail.com>
2022-07-06 01:04:53 -0600

    Added jumping

--- Platformer-Game/Assets/Scripts/PlayerMovement.cs
+++ Platformer-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Variables
     public float speed = 10f;
+    public float jumpForce = 10f;
+    private Rigidbody2D playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -23,5 +27,13 @@ public class PlayerMovement : MonoBehaviour
         float hi = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * hi * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
