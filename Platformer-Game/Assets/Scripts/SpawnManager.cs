b21748cdf73b9992823b92149e0f62fbceb08649
Luth <noafowl@gmail.com>
2022-07-20 15:06:20 -0600

    Added a couple of vars to spawn manager
    The prefab and spawn position of the player

--- Platformer-Game/Assets/Scripts/SpawnManager.cs
+++ Platformer-Game/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Variables 
+    public GameObject playerPrefab;
+    private Vector2 spawnPos = new Vector2(-2.73f, 0.28f);
+
     // Start is called before the first frame update
     void Start()
     {
